499197d806c9197789348a8e476fa9a083d5b8ce
gunthur.wibisono <gunthur.wibisono@greatdayhr.com>
2026-05-15 14:48:06 +0700

    Start Learn Hello World C#

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello" + 33);
+            Console.WriteLine(0.5 + 33);
+            // Console.Write(0.5 + 33); This line is comment and not will be executed
+            /**Console.Write("eee" + 33);
+            this is multiple comments, also not will be executed
+            Console.Write("eee" + "eee");
+            **/ 
+        }
+    }
+}
