2db7cd44212cf636c3652c484e9ba11d1929da42
gunthur.wibisono <gunthur.wibisono@greatdayhr.com>
2026-05-15 16:55:52 +0700

    add logical, comparison to operatorInAction

--- operatorInAction.cs
+++ operatorInAction.cs
@@ -42,4 +42,29 @@ Console.WriteLine("the value of a after reassign <<= 2 : {0}",a);
 a >>= 2;
 Console.WriteLine("the value of a after reassign >>= 2 : {0}",a);
 
-Console.WriteLine(repeatedChars + repeatedChars);
+Console.WriteLine(repeatedChars + "Comparison Operators" + repeatedChars);
+
+int c = 5;
+int b = 4;
+Console.WriteLine("the value of c is: {0}",c);
+Console.WriteLine("the value of b is: {0}",b);
+Console.WriteLine("c == b: 	"+(c==b));
+Console.WriteLine("c != b: 	"+(c!=b));
+Console.WriteLine("c < b:  \t"+(c<b));
+Console.WriteLine("c > b:  \t"+(c>b));
+Console.WriteLine("c <= b: \t"+(c<=b));
+Console.WriteLine("c >= b: \t"+(c>=b));
+
+Console.WriteLine(repeatedChars + "Logical Operators" + repeatedChars);
+
+bool isSunny = true;
+bool isWarm = false;
+
+Console.WriteLine("the value of isSunny is:\t{0}",isSunny);
+Console.WriteLine("the value of isWarm is:\t\t{0}",isWarm);
+Console.WriteLine("isSunny && isWarm:\t\t"+(isSunny && isWarm));
+Console.WriteLine("isSunny || isWarm:\t\t"+(isSunny || isWarm));
+Console.WriteLine("!isSunny:\t\t\t"+(!isSunny));
+Console.WriteLine("!isWarm:\t\t\t"+(!isWarm));
+
+Console.WriteLine(repeatedChars + "Ternary Operators" + repeatedChars);
