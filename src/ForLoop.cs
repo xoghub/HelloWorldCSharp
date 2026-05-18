40e7c0970d83962ce06ae09f7e6ea06b600b6f39
gunthur.wibisono <gunthur.wibisono@greatdayhr.com>
2026-05-18 10:39:47 +0700

    revise forloop

--- src/ForLoop.cs
+++ src/ForLoop.cs
@@ -1,3 +1,35 @@
-for (int i = 0; i < 10; i++){
-    Console.WriteLine(i);
+using System;
+
+namespace HelloWorld
+{
+    class ForLoop
+    {
+        public static void Main(string[] args)
+        {
+            // forLoopFunction();
+            forLoopCountDown();
+        }
+
+        public static void forLoopFunction()
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int index = 1; index <= number; index++)
+            {
+                Console.WriteLine(index);
+            }
+        }
+
+        public static void forLoopCountDown() 
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int index = number; index >= 0; index--)
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
 }
