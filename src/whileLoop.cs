eca317ab65ebb95c56e8125155af2cb053c82c7f
gunthur.wibisono <gunthur.wibisono@greatdayhr.com>
2026-05-18 10:29:32 +0700

    add whileLoop Example

---
+++ src/whileLoop.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace HelloWorldCSharp
+{
+    class WhileLoop
+    {
+        public static void Main(string[] args)
+        {
+            whileLoopNumberFunction();
+            whileLoopInputNameFunction();
+        }
+
+        public static void whileLoopNumberFunction()
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < number)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        public static void whileLoopInputNameFunction()
+        {
+            string name;
+
+            Console.Write("Enter your name: ");
+            name = Convert.ToString(Console.ReadLine());
+
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.Write("Enter your name: ");
+                name = Convert.ToString(Console.ReadLine());
+            }
+
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
